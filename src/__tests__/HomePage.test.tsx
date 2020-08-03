a79b5d404e580c9187605dddd759770a3c0c4162
Johann <johanson88@gmail.com>
2020-08-03 23:01:45 +0200

    Testing: Welcome test passing

--- src/__tests__/HomePage.test.tsx
+++ src/__tests__/HomePage.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import HomePage from './../pages/Homepage';
 
 test("Renders h1 message", () => {
     const { getByTestId } = render(
