42d40a9334616a72603fb15d77a155d9be7866f9
Johann <johanson88@gmail.com>
2020-08-03 23:19:34 +0200

    chore: add search user bar

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -2,7 +2,14 @@ import React from "react";
 
 const HomePage: React.FC<{}> = () => {
     return(
-        <h1 data-testid="homepage__title">Welcome</h1>
+        <>
+            <h1 data-testid="homepage__title">Welcome</h1>
+
+            <form>
+                <label htmlFor="user-searchbar">Introduce a Github username:</label>
+                <input type="text" id="user-searchbar"/>
+            </form>
+        </>
     )
 }
 
