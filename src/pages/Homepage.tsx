78873e2df9395ed8669fe5ac9288230f38686f09
Johann <johanson88@gmail.com>
2020-08-03 23:58:48 +0200

    Feat: add onChangeEvent to userSearchBar

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 const HomePage: React.FC<{}> = () => {
+
+    const [username, setUserName] = useState('');
+
     return(
         <>
             <h1 data-testid="homepage__title">Welcome</h1>
 
             <form>
                 <label htmlFor="user-searchbar">Introduce a Github username:</label>
-                <input type="text" id="user-searchbar"/>
+                <input
+                    type="text"
+                    id="user-searchbar"
+                    value={username}
+                    placeholder="Introduce a Github Username"
+                    onChange={ (
+                        e: React.ChangeEvent<HTMLInputElement>,
+                    ): void => setUserName(e.target.value) }
+
+                />
             </form>
         </>
     )
