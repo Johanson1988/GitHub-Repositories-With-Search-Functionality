33c016d47a97ec9ddbe761d9aeedc9ced3102dc4
Johann <johanson88@gmail.com>
2020-08-04 21:00:32 +0200

    Testing: Add test for (mock) API calls

--- src/__tests__/HomePage.test.tsx
+++ src/__tests__/HomePage.test.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, wait, getByTestId } from '@testing-library/react';
 import HomePage from './../pages/Homepage';
+import axios from 'axios';
+import MockAxios from 'axios-mock-adapter';
+
+const mock = new MockAxios(axios, { delayResponse: Math.random() * 500 });
 
 test("Renders h1 message", () => {
     const { getByTestId } = render(
@@ -10,12 +14,23 @@ test("Renders h1 message", () => {
 });
 
 test("Input search bar recieves user input", () => {
-    const { getByLabelText } = render(
+    mock.onGet().reply(200, {
+        login: "johanson1988"
+    });
+
+    const { getByLabelText, queryByText, getByTestId } = render(
         <HomePage />
+
+
     )
     const input = getByLabelText("Introduce a Github username:");
     expect(input).toBeInTheDocument();
-    fireEvent.change(input, { target: { value: 'a'} });
-    expect(input.value).toBe('a');
+    fireEvent.change(input, { target: { value: 'johanson1988'} });
+    expect(input.value).toBe('johanson1988');
+    const searchBarForm = getByTestId("user-searchbar");
+    fireEvent.submit(searchBarForm);
+
+    wait (() => expect(queryByText("Johanson1988")).toBeInTheDocument());
+    wait (() => expect(queryByText("404 NOT FOUND")).not.toBeInTheDocument());
 
 });
