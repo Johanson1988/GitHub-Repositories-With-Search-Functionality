9433c7c0dbce73ab41447bdd09dee12f0ec3885f
Johann <johanson88@gmail.com>
2020-08-04 19:40:43 +0200

    Feat: Home displaying username or 404

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -5,21 +5,24 @@ import { getUsersData } from '../helpers/API';
 const HomePage: React.FC<{}> = () => {
     // eslint-disable-next-line
     const [username, setUserName] = useState('');
-    const [userData, setUserData] = useState(null);
+    const [userData, setUserData] = useState({login: ''});
 
     const getUserName = async(username: string) => {
-        if (username.length > 4) {
+        if (username.length > 3) {
             setUserName(username);
             const userData = await getUsersData(username);
             setUserData(userData);
         }
     }
-
+    console.log(userData);
     return(
         <>
             <h1 data-testid="homepage__title">Welcome</h1>
 
             <UserSearchBar findUser={getUserName} />
+            {
+                username ? userData ? <h3>{userData.login}</h3> : <h3>404 NOT FOUND</h3> : null
+            }
         </>
     )
 }
