3b54927c2ae81f2092945acd872a43ea9df90b56
Johann <johanson88@gmail.com>
2020-08-04 23:28:43 +0200

    testing: Add User details tests without checking repositories yet

--- src/__tests__/HomePage.test.tsx
+++ src/__tests__/HomePage.test.tsx
@@ -3,6 +3,7 @@ import { render, fireEvent, wait } from '@testing-library/react';
 import HomePage from './../pages/Homepage';
 import axios from 'axios';
 import MockAxios from 'axios-mock-adapter';
+jest.setTimeout(30000);
 
 const mock = new MockAxios(axios, { delayResponse: Math.random() * 500 });
 afterAll(() => mock.restore());
@@ -32,33 +33,38 @@ test("Input search bar recieves user input", () => {
 
 });
 
-test("Renders user details after submitting a valid username", () => {
+test("Renders user details after submitting a valid username", async() => {
     mock.onGet().reply(200, {
         login: "johanson1988"
     });
 
-    const { queryByText, getByTestId } = render(
+    const { queryByText, getByTestId, getByAltText } = render(
         <HomePage />
     );
 
     const searchBarForm = getByTestId("user-searchbar");
+    expect(queryByText("johanson1988")).not.toBeInTheDocument();
     fireEvent.submit(searchBarForm);
-
-    wait (() => expect(queryByText("Johanson1988")).toBeInTheDocument());
-    wait (() => expect(queryByText("404 NOT FOUND")).not.toBeInTheDocument());
+    
+    await wait (() => expect(queryByText("johanson1988")).toBeInTheDocument());
+    await wait (() => expect(queryByText("404 NOT FOUND")).not.toBeInTheDocument());
+    await wait (() => expect(getByAltText("user-profile")).toBeInTheDocument());
+    await wait (() => expect(queryByText("Repositories")).toBeInTheDocument());
 
 });
 
-test("Renders 404 message after submitting invalid username", () => {
+test("Renders 404 message after submitting invalid username", async() => {
     mock.onGet().reply(200, null);
-    const { queryByText, getByTestId } = render(
+    const { queryByText, getByTestId, } = render(
         <HomePage />
     );
 
     const searchBarForm = getByTestId("user-searchbar");
     fireEvent.submit(searchBarForm);
 
-    wait (() => expect(queryByText("Johanson1988")).not.toBeInTheDocument());
-    wait (() => expect(queryByText("404 NOT FOUND")).toBeInTheDocument());
+    await wait (() => expect(queryByText("Johanson1988")).not.toBeInTheDocument());
+    await wait (() => expect(queryByText("404 NOT FOUND")).toBeInTheDocument());
+
+    //TODO Añadir test que compare la longitud de la lista de repositorios con la longitud del objeto de la API
 
 });
