dd9e4a7e73ea1813f732a8d182f21aa88ea0d9e7
Johann <johanson88@gmail.com>
2020-08-04 22:25:23 +0200

    Testing: Add test to check whether componentes displayed after api call

--- src/__tests__/HomePage.test.tsx
+++ src/__tests__/HomePage.test.tsx
@@ -13,20 +13,33 @@ test("Renders h1 message", () => {
     expect(getByTestId("homepage__title")).toHaveTextContent("Welcome");
 });
 
-test("Input search bar recieves user input", () => {
-    mock.onGet().reply(200, {
-        login: "johanson1988"
-    });
 
+
+test("Input search bar recieves user input", () => {
+    
     const { getByLabelText, queryByText, getByTestId } = render(
         <HomePage />
-
-
-    )
+    );
     const input = getByLabelText("Introduce a Github username:");
     expect(input).toBeInTheDocument();
     fireEvent.change(input, { target: { value: 'johanson1988'} });
     expect(input.value).toBe('johanson1988');
+    
+    expect(queryByText("Johanson1988")).not.toBeInTheDocument();
+    expect(queryByText("404 NOT FOUND")).not.toBeInTheDocument();
+    
+
+});
+
+test("Renders username after submitting a valid username", () => {
+    mock.onGet().reply(200, {
+        login: "johanson1988"
+    });
+
+    const { queryByText, getByTestId } = render(
+        <HomePage />
+    );
+
     const searchBarForm = getByTestId("user-searchbar");
     fireEvent.submit(searchBarForm);
 
