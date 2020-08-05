48d7864b225a5d2b923da93cfe62715247bf9312
Johann <johanson88@gmail.com>
2020-08-05 12:02:30 +0200

    feat: display repositories

--- src/components/Repositories.tsx
+++ src/components/Repositories.tsx
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import { getReposData } from './../helpers/API';
 
 type Props = { username: string };
 
+type ReposArray = { name: string, description: string}[];
+
 const Repositories: React.FC<Props> = ({ username }) => {
 
+    const [reposList, setReposList] = useState<ReposArray | null>(null);
+
+    useEffect(() => {
+        // Using an IIFE
+        (async function anyNameFunction() {
+            
+            setReposList(await getReposData(username));
+      })();
+    // eslint-disable-next-line    
+    }, []);
+    console.log(reposList)
+
     return(
+        <>
         <h4>Repositories</h4>
+        <ul data-testid="repos-container">
+        {
+            Array.isArray(reposList) && reposList.map((repoElement, index) => {
+            return(
+                <li key={index} className={"repo-li-element"} >{repoElement.name}</li>
+            )
+            })
+        }
+        </ul>
+        </>
     )
 
 }
