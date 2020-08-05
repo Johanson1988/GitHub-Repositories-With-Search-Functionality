9a7828a398cd38e1e4921d6ab3b6120693a986f9
Johann <johanson88@gmail.com>
2020-08-05 10:50:20 +0200

    refactor: type API calls result

--- src/helpers/API.ts
+++ src/helpers/API.ts
@@ -1,14 +1,18 @@
 import axios from 'axios';
 
-export const getUsersData = (username: string):any => {
-    return axios.get(`https://api.github.com/users/${username}`)
+export const getUsersData = (username: string):any =>
+    axios.get(`https://api.github.com/users/${username}`)
         .then(data => {
             return {login: data.data.login, imgSrc: data.data.avatar_url}
         })
-        .catch(() => null)
-    };
+        .catch(() => null);
 
 export const getReposData = (username: string):any => 
     axios.get(`https://api.github.com/users/${username}/repos`)
-        .then(data => data.data)
-        .catch(() => null)
+        .then(data => {
+
+            return data.data.map((repo: { name: string; description: string; }) => {
+                return {name: repo.name, description: repo.description}
+            });
+        })
+        .catch(() => null);
