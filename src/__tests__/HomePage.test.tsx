99589edf1508b1879fda4cd9faa2f0579006f8f5
Johann <johanson88@gmail.com>
2020-08-05 12:02:13 +0200

    Testing: Add test for repositories

--- src/__tests__/HomePage.test.tsx
+++ src/__tests__/HomePage.test.tsx
@@ -5,6 +5,9 @@ import HomePage from './../pages/Homepage';
 import axios from 'axios';
 import MockAxios from 'axios-mock-adapter';
 import Repositories from './../components/Repositories';
+
+import exampleReposObject from '../helpers/example-repos-object';
+
 jest.setTimeout(30000);
 
 const mock = new MockAxios(axios, { delayResponse: Math.random() * 500 });
@@ -70,16 +73,26 @@ test("Renders 404 message after submitting invalid username", async() => {
 });
 
 test("Displays repos list after submitting valid username", async() => {
-    mock.onGet().reply(200, {
-        login: "johanson1988" //Lista de repos con name y description
-    });
+    mock.onGet().reply(200, exampleReposObject);
 
-    const { container, queryByTestId, } = render(
+    const { /*container,*/ queryByTestId, } = render(
         <Repositories username="Johanson1988" />
     );
 
-    const repoList = container.querySelectorAll(".repo-li-element");
-    //expect(queryByTestId(container, 'not-empty')).not.toBeEmpty()
-
+    
+    
     await wait (()=> expect(queryByTestId("repos-container")).not.toBeEmpty());
+    //const repoList = container.querySelectorAll(".repo-li-element");
+    //await wait(()=> expect(repoList.length).toBe(exampleReposObject.length));
+    //TODO ver si puedo hacer que este test funcione bien
+});
+
+test("Repos list is empty if submitted wrong user name", async() => {
+    mock.onGet().reply(200, []);
+
+    const { queryByTestId, } = render(
+        <Repositories username="Johanson1988lalalalalalalalala" />
+    );
+
+    await wait (()=> expect(queryByTestId("repos-container")).toBeEmpty());
 });
