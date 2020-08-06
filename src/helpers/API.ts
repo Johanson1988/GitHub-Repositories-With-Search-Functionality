df7815cb4ff0bddc6fe49a1fb733ac72f21ad2c9
Johann <johanson88@gmail.com>
2020-08-06 17:40:50 +0200

    feat: replace REST axios with GraphQL axios

--- src/helpers/API.ts
+++ src/helpers/API.ts
@@ -4,11 +4,28 @@ import axios from 'axios';
  * object is filtered to pass only username and avatar url. */
 
 export const getUsersData = (username: string):any =>
-    axios.get(`https://api.github.com/users/${username}`)
-        .then(data => {
-            return {login: data.data.login, imgSrc: data.data.avatar_url}
-        })
-        .catch(() => null);
+    axios({
+        method: 'post', //you can set what request you want to be
+        url: 'https://api.github.com/graphql',
+        data: { query: `query {
+
+            user(login:"${username}") {
+                login
+                avatarUrl
+                repositories(first: 100, orderBy: {field:NAME, direction:ASC}) {
+                    nodes {
+                        name
+                        description
+                        }
+                    }
+                }
+            }` 
+        },
+        headers: {
+            Authorization: 'Bearer dd6897540c7cf344b92716679173b905ddc7943d'
+        }
+    }).then(data => data.data.data.user)
+    .catch(() => null)
 
 /** @const  {function} getReposData API call to Github to get repositories data, then 
  * object is filtered to pass only name and description. */
