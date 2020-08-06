91e4f986be278ba9c668474189898b038a0fbe52
Johann <johanson88@gmail.com>
2020-08-06 11:45:25 +0200

    Styles: Add card format

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 
-const NotFound: React.FC<{}> = () => <h3>404 NOT FOUND</h3>;
+const NotFound: React.FC<{}> = () => 
+    <div className="card-panel teal">
+        <h3 className="white-text">404 NOT FOUND</h3>;
+    </div>
 
 export default NotFound;
